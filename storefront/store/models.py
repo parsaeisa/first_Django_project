190a96449af1fe552029b622c0a54af40539cc9f
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-10-23 02:16:46 +0330

    app (store) : add one to many rels

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.db.models.base import ModelState
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import EmailField
 
 # Create your models here.
+
+class Collection (models.Model):
+
+    title = models.CharField(max_length=255)
+
 class Product (models.Model):
     
     # sku = models.CharField(max_length=10 , primary_key=True)
@@ -12,7 +18,7 @@ class Product (models.Model):
     price = models.DecimalField(max_digits=6 , decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-
+    collection = models.ForeignKey(Collection , on_delete=models.PROTECT)
 
 class Customer (models.Model):
 
@@ -33,7 +39,6 @@ class Customer (models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1 , choices=MEMBERSHIP_CHOICES , default=MEMBERSHIP_BRONZE)
 
-
 class Order (models.Model):
 
     PAYMENT_STATUS_PENDING = 'P'
@@ -48,6 +53,25 @@ class Order (models.Model):
     
     placed_at = models.DateField(auto_now_add=True)
     payment_status = models.CharField(max_length=1 , choices=PAYMENT_STATUS)
+    customer = models.ForeignKey(
+        Customer , on_delete= models.PROTECT 
+    )
+
+class OrderItem(models.Model):
+    
+    order = models.ForeignKey(Order , on_delete=models.PROTECT)
+    product = models.ForeignKey(Product , on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6 , decimal_places=2)
+
+class Cart (models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem (models.Model):
+
+    cart = models.ForeignKey(Cart , on_delete=models.CASCADE)
+    product = models.ForeignKey(Product , on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
 
 class Address(models.Model):
     
