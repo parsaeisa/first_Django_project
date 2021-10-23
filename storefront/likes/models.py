79a92703e084bcc49a619cd44f468e45444c2011
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-10-23 21:48:01 +0330

    app (likes) : add likedItem model

--- storefront/likes/models.py
+++ storefront/likes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+class likedItem (models.Model): 
+
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    content_type  = models.ForeignKey(ContentType , on_delete=models.CASCADE)
+    product_id = models.PositiveIntegerField()
+    content_type = GenericForeignKey()
